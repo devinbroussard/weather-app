7f118f23648deefbe67428895c8bb970fbbc4457
Devin Broussard <devin.ray.broussard@gmail.com>
2022-09-09 13:22:39 -0500

    Working on WeatherMenu component

---
+++ src/components/WeatherMenu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WeatherMenu = () =>  {
+    return (
+        <div className='card w-50 mx-auto my-auto bg-light bg-gradient px-5 py-5'>
+            test
+        </div>
+    );
+};
+
+export default WeatherMenu;
