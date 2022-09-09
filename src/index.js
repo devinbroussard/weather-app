d14a43962cb6ce56bc1d2cf849c8b981ea9122b7
Devin Broussard <devin.ray.broussard@gmail.com>
2022-09-09 13:01:56 -0500

    Added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 import App from './components/App';
 import React from 'react';
